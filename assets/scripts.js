ce494634bcb79ce1799d4b8dde5267b5f3e67f9a
isabellacoraci <ic21353n@pace.edu>
2022-11-16 14:39:59 -0500

    commit 1 11-16

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,13 +11,20 @@ function addToCart(cookie) {
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
+   itemCount = itemCount + 1
+   console.log(itemCount)
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
+   if (cookie == 'peanut butter'){currentPrice = currentPrice + 20 }
+   if (cookie == 'sandies') {currentPrice = currentPrice + 30}
+   if (cookie == 'party press') {currentPrice = currentPrice + 35}
+   if (cookie == 'chocolate chip') {currentPrice = currentPrice + 25}
 
+   console.log(currentPrice)
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+    window.alert(`Item Count: ${itemCount} Total Cost: ${currentPrice}`)
 }
