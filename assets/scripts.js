8390c18dcb16abac110a884dbe069e783eb24876
isabellacoraci <ic21353n@pace.edu>
2022-12-05 09:25:06 -0500

    12-5 commit 1

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,18 @@
+const cart = {
+    currentPrice: 0,
+    items: [],
+    addItem: function(cookie, price) {
+        // add a cookie (string) to the items array
+
+        //add the price (number) to the currentPrice properties
+
+    },
+    clear: function() {
+        //reset the currentPrice and items properties
+        
+    },
+}
+
 let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
