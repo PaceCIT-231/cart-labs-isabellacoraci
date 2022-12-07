3200a59714cc29156d1981733d1d0945fa0b194c
isabellacoraci <ic21353n@pace.edu>
2022-12-07 12:38:21 -0500

    12-7 commit 2

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,17 +3,17 @@ const cart = {
     items: [],
     addItem: function(cookie, price) {
         // add a cookie (string) to the items array
-
+    this.items.push(cookie)
         //add the price (number) to the currentPrice properties
-
+    this.currentPrice= this.currentPrice + price 
     },
     clear: function() {
         //reset the currentPrice and items properties
-        
+        this.currentPrice= 0
+        this.items= []
     },
 }
 
-let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
     /* 
@@ -26,22 +26,21 @@ function addToCart(cookie) {
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
-   itemCount = itemCount + 1
-   console.log(itemCount)
 
-    document.getElementById("cartItems").innerHTML= itemCount
 
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
-   if (cookie == 'peanut butter'){currentPrice = currentPrice + 20 }
-   if (cookie == 'sandies') {currentPrice = currentPrice + 30}
-   if (cookie == 'party press') {currentPrice = currentPrice + 35}
-   if (cookie == 'chocolate chip') {currentPrice = currentPrice + 25}
+   if (cookie == 'peanut butter'){cart.addItem(cookie, 20)}
+   if (cookie == 'sandies') {cart.addItem(cookie, 30)}
+   if (cookie == 'party press') {cart.addItem(cookie, 35)}
+   if (cookie == 'chocolate chip') {cart.addItem(cookie, 25)}
 
-   document.querySelector(".hoverText").innerHTML = currentPrice
-
-   console.log(currentPrice)
+   document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
+
+
 
 function checkout() {
     console.log('User is checking out.')
